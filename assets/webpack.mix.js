fa7e72ae59c4af0a7efbd32893d2834259e8a722
Thế Anh Đặng <dangtheanh16@gmail.com>
2026-02-20 11:04:11 +0700

    feat(referral): add foundational files for the new referral module.

--- assets/webpack.mix.js
+++ assets/webpack.mix.js
@@ -11,12 +11,12 @@ mix.options({
     terser: {extractComments: false}
 });
 
-mix.setPublicPath(`public/modules/$LOWER_NAME$`);
+mix.setPublicPath(`public/modules/referral`);
 
 mix.styles([
     //
-], 'public/modules/$LOWER_NAME$/css/main.min.css');
+], 'public/modules/referral/css/main.min.css');
 
 mix.combine([
     //
-], 'public/modules/$LOWER_NAME$/js/main.min.js');
+], 'public/modules/referral/js/main.min.js');
